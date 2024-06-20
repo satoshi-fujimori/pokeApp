4094a32e1f6810c1a105ac6fecc67369d787af4a
satoshi <fuji02001458@gmail.com>
2024-06-21 08:55:45 +0900

    最初のコミット

---
+++ src/main/java/com/example/demo/PokeAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeAppApplication.class, args);
+	}
+
+}
